7ac3ed3729a3a1e299d6c32bfd4a9f766ba3c665
Vamsee Krishna <vamseekrishna9201@gmail.com>
2024-09-18 15:20:55 -0500

    efficient solution that was accepted

--- best_time_to_buy_and_sell_stock.py
+++ best_time_to_buy_and_sell_stock.py
@@ -4,29 +4,16 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        if len(prices) < 2:
-            return 0
-        buy = 0
-        sell = 1
-        maxProfit = -100000
-        highestSellIndex = len(prices) - 1
-        while sell < len(prices):
-            currentProfit = prices[sell] - prices[buy]
-            if currentProfit >= maxProfit:
-                maxProfit = currentProfit
-                highestSellIndex = sell
-            sell = sell+1
-        while buy < highestSellIndex:
-            currentProfit = prices[highestSellIndex] - prices[buy]
-            if currentProfit > maxProfit:
-                maxProfit = currentProfit
-            buy = buy + 1
-        if maxProfit > 0:
-            return maxProfit
-        else:
-            return 0
-
-            
+        l = 0
+        r = 1
+        maxP = 0
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                maxP = max(maxP,prices[r] - prices[l])
+            else :
+                l = r
+            r = r + 1  
+        return maxP
 
 
 
