634544bfde07acdcfa1d698200795e2f39659dfd
Vamsee Krishna <vamseekrishna9201@gmail.com>
2024-09-18 16:49:41 -0500

    more memory efficient algorithm

--- palindrome.py
+++ palindrome.py
@@ -4,10 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        newStr = ""
-        for c in s:
-            if c.isalnum():
-                newStr += c.lower()
-        return newStr == newStr[-1::-1]           
+        l,r = 0,len(s)-1
+        while l < r:
+            while l <r and not self.isAlphaNum(s[l]):
+                l +=1
+            while r>l and not self.isAlphaNum(s[r]):
+                r -=1
+            if s[l].lower() != s[r].lower():
+                return False
+            l+=1
+            r-=1
+        return True
+    def isAlphaNum(self,c):
+        return( (ord(c) >= 65 and ord(c) <= 90) or 
+        (ord(c) >= 97 and ord(c) <= 122) or  
+        (ord(c) >= 48  and ord(c) <= 57))
 
         
