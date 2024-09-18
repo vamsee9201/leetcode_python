7cefc9dc99ec8607138c91afe1fec41c52d465f1
Vamsee Krishna <vamseekrishna9201@gmail.com>
2024-09-18 16:31:40 -0500

    a cleaner way of writing code

--- palindrome.py
+++ palindrome.py
@@ -4,18 +4,10 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        print(ord("0"))
-        print(ord("9"))
-        check = ""
-        for i in range(len(s)):
-            if (ord(s[i]) >= 65 and ord(s[i]) <= 90) or (ord(s[i]) >= 97 and ord(s[i]) <= 122) or  (ord(s[i]) >= 48 and ord(s[i]) <= 57):
-                check = check + s[i]
-        check = lower(check)
-        print(check)
-        if check == check[-1::-1]:
-            return True
-        else:
-            return False
-                
+        newStr = ""
+        for c in s:
+            if c.isalnum():
+                newStr += c.lower()
+        return newStr == newStr[-1::-1]           
 
         
