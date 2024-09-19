7e678b49a87f2211e9f273ce9d18383a306083ba
Vamsee Krishna <vamseekrishna9201@gmail.com>
2024-09-19 15:50:23 -0500

    efficient nlogn solution

--- valid_anagram.py
+++ valid_anagram.py
@@ -5,15 +5,5 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        #hashmap solution
-        if len(s) != len(t):
-            return False
-        countS,countT = {},{}
-        for i in range(len(s)):
-            countS[s[i]] = countS.get(s[i],0) + 1
-            countT[t[i]] = countT.get(t[i],0) + 1
-        for c in countS:
-            if countS[c] != countT.get(c,0):
-                return False
-        return True
+        return sorted(s) == sorted(t)
         
