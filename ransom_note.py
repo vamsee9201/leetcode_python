cd3507e5c01822f4d8b79e83a1165f1cf6bdfbc2
Vamsee Krishna <vamseekrishna9201@gmail.com>
2024-09-22 16:00:00 -0500

    added an efficient solution

--- ransom_note.py
+++ ransom_note.py
@@ -5,18 +5,28 @@ class Solution(object):
         :type magazine: str
         :rtype: bool
         """
-        ransomMap = {}
-        magazineMap = {}
-        for i in range(len(ransomNote)):
-            ransomMap[ransomNote[i]] = ransomMap.get(ransomNote[i],0) + 1
-        for i in range(len(magazine)):
-            magazineMap[magazine[i]] = magazineMap.get(magazine[i],0) + 1
-        print(ransomMap)
-        print(magazineMap)
-        for c in ransomMap:
-            if magazineMap.get(c,0) < ransomMap[c]:
-                return False
-        return True
+        # ransomMap = {}
+        # magazineMap = {}
+        # for i in range(len(ransomNote)):
+        #     ransomMap[ransomNote[i]] = ransomMap.get(ransomNote[i],0) + 1
+        # for i in range(len(magazine)):
+        #     magazineMap[magazine[i]] = magazineMap.get(magazine[i],0) + 1
+        # print(ransomMap)
+        # print(magazineMap)
+        # for c in ransomMap:
+        #     if magazineMap.get(c,0) < ransomMap[c]:
+        #         return False
+        # return True
+        ran = sorted(list(ransomNote))
+        mag = sorted(list(magazine))
+
+        for char in mag:
+            if ran and char == ran[0]:
+                ran.pop(0)
+        if ran:
+            return False
+        else:
+            return True
 
             
 
