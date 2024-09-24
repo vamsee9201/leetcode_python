8292949bc7eeffbf95513190fc621d598f83feb2
Vamsee Krishna <vamseekrishna9201@gmail.com>
2024-09-23 22:48:41 -0500

    more efficient solution for longest palindrome

--- longest_palindrome.py
+++ longest_palindrome.py
@@ -10,23 +10,16 @@ class Solution(object):
         
         odd = False
         palindromeLength = 0
-        lowestOccuringEvenCharacter = 2001
         for key,value in stringMap.items():
-            if (not odd) and (value%2 != 0):
-                odd = True
-                palindromeLength += value
-            elif odd and (value%2 != 0):
+            if value%2:
                 palindromeLength += value-1
-            else:
-                pass
+                odd = True
             if value%2 == 0:
                 palindromeLength += value
-                if value < lowestOccuringEvenCharacter:
-                    lowestOccuringEvenCharacter = value
 
-        # if palindromeLength%2 == 0:
-        #     palindromeLength = palindromeLength - (lowestOccuringEvenCharacter - 1)
-        print(stringMap)
+
+        if odd:
+            return (palindromeLength + 1)
         return palindromeLength
 
         
