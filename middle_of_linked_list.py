730082f2ab217e716da1c1303b4c7e4aa930aaef
Vamsee Krishna <vamseekrishna9201@gmail.com>
2024-09-25 11:37:01 -0500

    efficient slow and fast pointer approach

--- middle_of_linked_list.py
+++ middle_of_linked_list.py
@@ -9,18 +9,12 @@ class Solution(object):
         :type head: ListNode
         :rtype: ListNode
         """
-        curr = head
-        total = 0
-        while curr:
-            total +=1
-            curr = curr.next
-        
-        target = total//2 + 1
-        curr = head
-        for i in range(1,total+1):
-            if i == target:
-                return curr
-            curr = curr.next
+        slow,fast = head,head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
             
 
         
