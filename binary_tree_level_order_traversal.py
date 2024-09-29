3ba6354efcf50214760fca00700620af991fbc84
Vamsee Krishna <vamseekrishna9201@gmail.com>
2024-09-28 22:20:21 -0500

    added binary tree level order traversal code

---
+++ binary_tree_level_order_traversal.py
@@ -0,0 +1,32 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def levelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+
+        queue = deque()
+        res = []
+        queue.append(root)
+
+        while queue:
+            qlen = len(queue)
+            level = []
+            for i in range(qlen):
+                node = queue.popleft()
+                if node:
+                    level.append(node.val)
+                    queue.append(node.left)
+                    queue.append(node.right)
+            if level:
+                res.append(level)
+        
+        return res
+
+        
