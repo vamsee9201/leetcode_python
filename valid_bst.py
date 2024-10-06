702e129b88af1d3b232117e04a6789c457de007d
vamsee9201 <vamseekrishna9201@gmail.com>
2024-10-05 22:43:30 -0500

    added a right solution

--- valid_bst.py
+++ valid_bst.py
@@ -10,44 +10,13 @@ class Solution(object):
         :type root: TreeNode
         :rtype: bool
         """
-
-        def dfs(root):
-           
-            if not root:
-                return [True,None,None,None]
-            print(root.val)
-
-            left,right = dfs(root.left),dfs(root.right)
-            if not left[1] or root.val > left[1]:
-                leftOk = True
-            else:
-                leftOk = False
-
-            if not right[1] or root.val < right[1]:
-                rightOk = True
-            else:
-                rightOk = False
+        def isValid(node,left,right):
+            if not node:
+                return True
             
-            if not left[2] or left[2] > left[1]:
-                leftOk = True
-                leftmin = left[1]
-            else:
-                leftOk = False
-                leftmin = left[1]
+            if not (node.val < right and node.val > left):
+                return False
             
-            if not right[3] or right[3] < right[1]:
-                rightOk = True
-                rightMax = right[1]
-            else:
-                rightOK = False
-                rightMax = right[1]
+            return isValid(node.left,left,node.val) and isValid(node.right,node.val,right)
 
-            isValid = (leftOk and rightOk) and (left[0] and right[0])
-            return (isValid,root.val,leftmin,rightMax)
-        return dfs(root)[0]
-        #return True
-
-
-            
-                
-        
+        return isValid(root,float("-inf"),float("inf")) 
