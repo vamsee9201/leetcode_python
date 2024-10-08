83fa678cb1af3feb184bb7bdfa78c4d6610231a6
vamsee9201 <vamseekrishna9201@gmail.com>
2024-10-08 12:51:06 -0500

    final solution

--- rotting_oranges.py
+++ rotting_oranges.py
@@ -5,34 +5,31 @@ class Solution(object):
         :rtype: int
         """
         rows,cols = len(grid),len(grid[0])
+
+
+        q = collections.deque()
         visit = set()
-        sq = collections.deque()
+        fresh = 0
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 2 and (r,c) not in visit:
-                    sq.append((r,c))
+                    q.append((r,c))
                     visit.add((r,c))
+                if grid[r][c] == 1:
+                    fresh += 1
+
+        directions = [[1,0],[-1,0],[0,1],[0,-1]]
         minutes = 0
-        print(sq)
-        while sq:
-            row,col = sq.popleft()
-            directions = [[1,0],[-1,0],[0,1],[0,-1]]
-            for dr,dc in directions:
-                r,c = row + dr,col + dc
-                if r in range(rows) and c in range(cols) and grid[r][c] == 1 and (r,c) not in visit:
-                    sq.append((r,c))
-                    visit.add((r,c))
-            print(sq)
+        while q and fresh > 0:
+            for i in range(len(q)):
+                row,col = q.popleft()
+                for dr,dc in directions:
+                    r,c = row + dr, col + dc
+                    if r in range(rows) and c in range(cols) and grid[r][c] == 1 and (r,c) not in visit:
+                        q.append((r,c))
+                        visit.add((r,c))
+                        fresh -= 1
             minutes += 1
-            print(minutes)
-        return minutes
+        return minutes if fresh == 0 else -1
 
-
-        
-
-        
-
-        
-
-                
         
