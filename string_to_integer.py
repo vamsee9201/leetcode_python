7c49561601e5d378cca9aff503ed8e78ce650c73
vamsee9201 <vamseekrishna9201@gmail.com>
2024-10-22 20:15:32 -0500

    found a solution

--- string_to_integer.py
+++ string_to_integer.py
@@ -4,4 +4,30 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        s = s
+        s = s.strip()
+
+        result = 0
+        sign = 1
+
+        if not s:
+            return 0
+
+        if s[0] == "-" or s[0] == "+":
+            if s[0] == "-":
+                sign = -1
+            s = s[1:]
+        
+        for char in s:
+            if not char.isdigit():
+                break
+            result = result*10 + int(char)
+        
+        result = result*sign
+
+        if result < -2**31:
+            return -2**31
+        elif result > 2**31 -1:
+            return 2**31 - 1
+        
+
+        return result
