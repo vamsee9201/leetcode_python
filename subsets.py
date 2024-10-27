47383ac6a5ba67011a4659431ee0818b1ad3994c
vamsee9201 <vamseekrishna9201@gmail.com>
2024-10-26 21:02:00 -0500

    subsets

--- subsets.py
+++ subsets.py
@@ -4,4 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
-        
+        subset = []
+        for i in range(len(nums)):
+            for j in range(len(nums)):
+                if j > i:
+                    toAdd = [nums[i],nums[j]]
+                    subset.append(toAdd)
+        for n in nums:
+            subset.append([n])
+        subset.append([])
+        if nums not in subset :
+            subset.append(nums)
+        else:
+            pass
+        return subset
