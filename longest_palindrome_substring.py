4e98ca26207f3736c144094609aa9f2b08953bcf
vamsee9201 <vamseekrishna9201@gmail.com>
2024-10-30 16:00:04 -0500

    added the longest palindrome substring

--- longest_palindrome_substring.py
+++ longest_palindrome_substring.py
@@ -4,4 +4,27 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        
+        res = ""
+        resLen = 0
+
+        for i in range(len(s)):
+            l,r = i,i
+            while l >= 0 and r <= len(s)-1 and s[l] == s[r]:
+                newLen = r - l + 1
+                if newLen > resLen:
+                    resLen = newLen
+                    res = s[l:r+1]
+                l -= 1
+                r += 1
+
+            l,r = i,i+1
+            while l >= 0 and r <= len(s)-1 and s[l] == s[r]:
+                newLen = r - l + 1
+                if newLen > resLen:
+                    resLen = newLen
+                    res = s[l:r+1]
+                l -= 1
+                r += 1
+
+
+        return res
