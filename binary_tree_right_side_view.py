89a49e8d1915dd00a6e33d66dc98349103cdffdb
vamsee9201 <vamseekrishna9201@gmail.com>
2024-10-29 23:10:27 -0500

    added solution for binary tree right side view

--- binary_tree_right_side_view.py
+++ binary_tree_right_side_view.py
@@ -10,4 +10,21 @@ class Solution(object):
         :type root: Optional[TreeNode]
         :rtype: List[int]
         """
+        res = []
+        q = collections.deque([root])
+
+        while q:
+            rightSide = None
+            qlen = len(q)
+
+            for i in range(qlen):
+                node = q.popleft()
+                if node:
+                    rightSide = node
+                    q.append(node.left)
+                    q.append(node.right)
+                
+            if rightSide:
+                res.append(rightSide.val)
         
+        return res
