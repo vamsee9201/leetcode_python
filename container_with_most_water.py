865a78a4734116850319751258b108829ec17f64
vamsee9201 <vamseekrishna9201@gmail.com>
2024-11-12 21:05:09 -0600

    added a solution

--- container_with_most_water.py
+++ container_with_most_water.py
@@ -4,4 +4,22 @@ class Solution(object):
         :type height: List[int]
         :rtype: int
         """
+        l = 0
+        r = len(height)-1
+
+        area = -1
+        while l < r:
+            b = min(height[l],height[r])
+            length = r - l
+            currentArea = length*b
+            if currentArea > area:
+                area = currentArea
+
+            if height[l] < height[r]:
+                l += 1
+            else:
+                r -= 1
+        return area
+            
+
         
