90800e29df55ab9c6e36024a4684c60662b8d8b5
vamsee9201 <vamseekrishna9201@gmail.com>
2024-11-19 21:54:54 -0600

    added solution

--- letter_combinations_phone_number.py
+++ letter_combinations_phone_number.py
@@ -4,4 +4,27 @@ class Solution(object):
         :type digits: str
         :rtype: List[str]
         """
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz"
+        }
+
+        def backTrack(i,curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backTrack(i+1,curStr+c)
+        
+        if digits:
+            backTrack(0,"")
+
+        return res
         
