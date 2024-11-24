80659298ecb615d3da0c8b97335696c5cdd3e3ae
vamsee9201 <vamseekrishna9201@gmail.com>
2024-11-24 16:44:33 -0600

    added code

--- find_anagrams_in_a_string.py
+++ find_anagrams_in_a_string.py
@@ -5,7 +5,24 @@ class Solution(object):
         :type p: str
         :rtype: List[int]
         """
-        checkSet = set()
-        res = []
-        for c in s:
-            
+        if len(p) > len(s) :
+            return []
+        
+        sCount,pCount = {},{}
+
+        for i in range(len(p)):
+            pCount[p[i]] = pCount.get(p[i],0) + 1
+            sCount[s[i]] = sCount.get(s[i],0) + 1
+        
+        res = [0] if pCount == sCount else []
+        l = 0
+        for i in range(len(p),len(s)):
+            sCount[s[i]] = sCount.get(s[i],0) + 1
+            sCount[s[l]] -= 1
+
+            if sCount[s[l]] == 0:
+                sCount.pop(s[l])
+            l += 1
+            if sCount == pCount:
+                res.append(l)
+        return res
